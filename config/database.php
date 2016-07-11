27b40c427ce5fb86c24ad50b3c741bfb9ba1edbf
Antonio Baez <antonio.baez@advanzer.com>
2016-07-11 15:04:16 -0500

    database

--- config/database.php
+++ config/database.php
@@ -54,7 +54,7 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'localhost'),
+            'host' => env('DB_HOST', '192.168.0.103'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'root'),
             'username' => env('DB_USERNAME', 'root'),
