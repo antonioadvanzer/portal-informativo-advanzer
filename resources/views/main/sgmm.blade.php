e051434996cdb81588787cc8ba2f24cdfdebbe6f
Antonio Baez <antonio.baez@advanzer.com>
2016-10-21 12:36:25 -0500

    Cambio en información en seccion de Seguro Axxa

--- resources/views/main/sgmm.blade.php
+++ resources/views/main/sgmm.blade.php
@@ -18,31 +18,23 @@
         <div class="services-half-width-container">
         	<div class="container">
 	            <div class="row">
-	                <div class="col-sm-6 services-half-width-text wow fadeInLeft">
+	                <div class="col-sm-6 services-full-width-text wow fadeInLeft">
 	                    <h3>1. ¿Cómo reporto mi siniestro?</h3>
 	                    
                         <p>
-	                    	Llama al 01 800 201 6764 línea exclusiva para reporte de Siniestros GMM, disponible las 24 horas los 365 días del año.
+	                    	Ante cualquier necesidad médica llamar al <b>01 (800) 911 9999</b>: Asistencia médica telefónica las 24 horas.
                             <br/>
                             <strong>Orientación en caso de enfermedad o accidente:</strong>
                             
 	                    </p>
                         
-                        <ul>
-                            <li>Centros de Atención Médica</li>
-                            <li>Grupo de Especialistas Médicos</li>
-                            <li>Coordinación Internamiento Centro Hospitalario</li>
-                            <li>Información servicios médicos</li>
-                            <li>Quejas y Comentarios</li>
-                        </ul>
-                        
-                        <p>Importante. Favor de tener a la mano tu número de póliza (0293006) y certificado. Estos datos aparecen en tu credencial.
+                        <p><b>Importante:</b> Favor de tener a la mano tú número de póliza (FW50667A) y certificado.
                         </p>
                         
                         <img src="{{ URL::to('img/sgmm/Foto-Autos.jpg') }}" style="max-width:400px;" width="100%" class="img-fluid">
                         
 	                </div>
-	                <div class="col-sm-6 services-half-width-text wow fadeInUp">
+	                <div class="col-sm-6 services-full-width-text wow fadeInUp">
 	                    <h3>2. ¿Qué puedo encontrar en <a href="http://www.axa.mx" target="_blank">www.axa.mx</a>?</h3>
 	                    
                         <p>Directorio médico, directorio de hospitales, informe médico, solicitud de rembolso, solicitud de programación, solicitud de pago directo.</p>
@@ -65,43 +57,49 @@
 	                    	
 	                	</p>
 	                </div>
+                    
                     <div class="col-sm-6 services-half-width-text wow fadeInUp">
-	                    <h3>Programaci&oacute;n Cirug&iacute;a</h3>
+	                    <h3>Rembolso</h3>
+                        
+                        <h4>Solicita al médico que te atendió, que llene el Informe Médico de AXA.</h4>
 	                    
-                        <ul> 
-                            <li>Llenado de INFORME MEDICO (con diagnóstico definitivo).</li>
+                        <ul type="1"> 
+                            <li>Llenado por el asegurado el formato de  rembolso de gastos médicos.</li>
 
-                            <li>Llenado de INFORME DE RECLAMANTE (datos de contacto).</li>
+                            <li>Identificación oficial del paciente.</li>
 
-                            <li>Copia de identificaciones del asegurado afectado</li>
+                            <li>Documentos fiscales de documentos a rembolsar.
+                            <br>(Para las facturas de estudios favor de  anexar resultados o interpretaciones médicas para facturas de medicamentos anexar copia de la receta médica).</li>
 
-                            <li>(IFE, FM3 o Pasaporte y Credencial Banorte).</li>
+                            <li>AXA rembolsa tus gastos a través de transferencia electrónica bancaria o con una orden de pago. Es importante mencionar en la Solicitud de Rembolso el medio de pago en que deseas que AXA te rembolse tus gastos. En caso de solicitar transferencia electrónica, anexar copia de estado de cuenta.</li>
 
-                            <li>Interpretación de estudios, en caso de programación de medicamentos, copia de receta médica (la original se entregará al recibir medicamento).</li>
+                            <li>Comprobante de domicilio.</li>
                             
-                            <li>Lugar: Oficina Seguros Banorte-Generali, Médico Coordinador plaza, asesor o agente o directamente al email: siniestros@ers.com.mx y tel. (0181) 8242-2735.</li>
+                        </ul>
+                        
+                        <h4><b>Lugar para entrega de documentos: En el módulo más cercano de AXA, asesor o agente, o directamente al email siniestros@ers.com.mx <br> Tel: 0181-8242-2735 Ext 131</b></h4>
+	                </div>
+                    
+                    <div class="col-sm-6 services-half-width-text wow fadeInUp">
+	                    <h3>Programaci&oacute;n de Servicios</h3>
+                        
+                        <h4>Para esta solicitud, es necesario que presentes toda la documentación necesaria al menos cinco días antes de la cirugía:</h4>
+	                    
+                        <ul type="1"> 
+                            <li>Solicitud de programación de servicios médicos.</li>
+
+                            <li>Informe médico.</li>
+
+                            <li>Copia de los estudios de laboratorio y/o gabinete, con su interpretación en donde se haya confirmado el diagnóstico.</li>
+
+                            <li>Copia de la tarjeta del seguro o póliza.</li>
+
+                            <li>Copia de identificación oficial del paciente.</li>
+                            
                         </ul>
                         
 	                </div>
                     
-                    <div class="col-sm-6 services-half-width-text wow fadeInUp">
-	                    <h3>Reporte Hospitalario</h3>
-	                    
-                        <ul> 
-                            <li>Acudir al hospital de red (es indispensable presentar identificación oficial del asegurado).</li>
-
-                            <li>El Hospital reporta ingreso a la Aseguradora a través de CAT.</li>
-
-                            <li>Durante las primeras 24 horas del aviso lo visitará Ejecutivo de Servicio o Médico.</li>
-
-                            <li>Coordinador de Plaza para asesoría e integración de información.</li>
-
-                            <li>Una vez obtenida la información completa se otorga la carta cobertura la cual incluye: Monto Autorizado Hospital/Honorarios autorizados al equipo Médico/Deducible/Coaseguro.</li>
-                            
-                        </ul>
-            
-	                </div>
-                
 	            </div>
 	        </div>
         </div>
@@ -112,27 +110,56 @@
 	            <div class="row">
 	                
                     <div class="col-sm-6 services-half-width-text wow fadeInUp">
-	                    <h3>Reembolso</h3>
+	                    <h3>Urgencia Médica</h3>
 	                    
-                        <ul> 
-                            <li>Llenado de INFORME MEDICO (debe ser actualizado cada 3 meses).</li>
+                        <h4>A continuación te presentamos los pasos que debes seguir en caso que tengas una urgencia médica:</h4>
+                        
+                        <ul type="1"> 
+                            <li>Acude al área de urgencias en algún hospital en convenio.</li>
 
-                            <li>Copia de Identificación oficial y Credencial Banorte.</li>
+                            <li>El hospital reporta tu ingreso a través de nuestro Centro de Contacto.</li>
 
-                            <li>Documentos fiscales de documentos a reembolsar (facturas, recibos de honorarios a nombre del Titular de póliza o de Banorte Generali).</li>
-
-                            <li>Carta de autorización de Transferencia.</li>
-
-                            <li>Copia de Estado de Cuenta con CLABE legible.</li>
+                            <li>Los documentos que debes presentar, en el módulo hospitalario de AXA o con alguno de nuestros representantes médicos:
                             
+                                <ul>
+                                    <li>Solicitud de pago directo.</li>
+                                    <li>Informe Médico.</li>
+                                    <li>Credencial de AXA o póliza.</li>
+                                    <li>Identificación oficial vigente del paciente.</li>
+                                </ul>
+                            </li>
+                            <li>AXA determinará si tu atención médica de urgencia está cubierta por tu póliza en un lapso de 48 horas entregándole carta de autorización, donde indicara monto autorizado, hospital, honorarios autorizados al equipo médico, deducible y coaseguro.</li>
+                            <li>Antes de salir del hospital, tienes que liquidar tu deducible, coaseguro y/ o gastos no cubiertos por tu póliza. </li>
                         </ul>
                     
 	                </div>
                     <div class="col-sm-6 services-half-width-text wow fadeInUp">
-	                    <h3></h3>
+	                    <h3>Facturaci&oacute;n</h3>
 	                    
-                        <img src="{{ URL::to('img/sgmm/vias-de-reclamacion-668x400x80xX.jpg') }}" style="max-width:400px;" width="100%" class="img-fluid">
-                    
+                        <h4>Para gastos médicos distintos a enfermería, honorarios médicos y medicamentos; el recibo o factura debe venir a nombre de AXA Seguros.
+                        <br>Los datos fiscales son:</h4>
+                        
+                        <table class="responstable" style="width:100%">
+                            <tr>
+                                <td>
+                                    <ul>
+                                        <li>Razón social: AXA SEGUROS S.A. DE C.V.</li>
+
+                                        <li>RFC: ASE931116231</li>
+
+                                        <li>Domicilio fiscal: 
+                                            <br>Av. Félix Cuevas 366, Piso 6
+
+                                            Col. Tlacoquemécatl
+
+                                            Del. Benito Juárez, 03200, México, DF
+                                        </li>
+                                    </ul>
+                                    
+                                </td>
+                            </tr>
+                        </table>
+                        
 	                </div>
                     
 	            </div>
@@ -148,7 +175,7 @@
                         <ul>
                             <li>
                                 <i class="fa fa-file-pdf-o"></i>
-                                <a target="_blank" href="https://docs.google.com/a/advanzer.com/viewer?a=v&pid=sites&srcid=YWR2YW56ZXIuY29tfHBvbGl0aWNhcy1yaHxneDo1NjdmZDZhMmQ5YTEyYTAw">CONDICIONES GENERALES AXA (LINEA HOSP)</a>
+                                <p><a target="_blank" href="https://docs.google.com/a/advanzer.com/viewer?a=v&pid=sites&srcid=YWR2YW56ZXIuY29tfHBvbGl0aWNhcy1yaHxneDo1NjdmZDZhMmQ5YTEyYTAw">CONDICIONES GENERALES AXA (LINEA HOSP)</a></p>
                             </li>
                             <li>
                                 <i class="fa fa-file-pdf-o"></i>
